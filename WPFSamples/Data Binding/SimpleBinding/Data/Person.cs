cda46360d49d3210eaacee7ee3d62e652d4a9ad1
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-11-22 15:30:46 -0500

    Simple Binding

---
+++ WPFSamples/Data Binding/SimpleBinding/Data/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace SimpleBinding.Data
+{
+    public class Person : INotifyPropertyChanged
+    {
+
+
+        private string _name;
+
+        public Person()
+        {
+        }
+
+        public Person(string value)
+        {
+            _name = value;
+        }
+
+        public string PersonName
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged("PersonName");
+            }
+        }
+
+        // Declare the event
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
