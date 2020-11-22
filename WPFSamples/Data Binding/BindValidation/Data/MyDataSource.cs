8b0d37007ddf2caf16123adacb683a87e053a30d
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-11-21 21:09:51 -0500

    Bind Validation sample.

---
+++ WPFSamples/Data Binding/BindValidation/Data/MyDataSource.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindValidation.Data
+{
+    public class MyDataSource
+    {
+        public MyDataSource()
+        {
+            Age = 0;
+            Age2 = 0;
+        }
+
+        public int Age { get; set; }
+        public int Age2 { get; set; }
+        public int Age3 { get; set; }
+    }
+}
--- WPFSamples/Data Binding/BindValidation/MainWindow.xaml.cs
+++ WPFSamples/Data Binding/BindValidation/MainWindow.xaml.cs
@@ -24,5 +24,26 @@ namespace BindValidation
         {
             InitializeComponent();
         }
+
+
+        private void UseCustomHandler(object sender, RoutedEventArgs e)
+        {
+            var myBindingExpression = textBox3.GetBindingExpression(TextBox.TextProperty);
+            var myBinding = myBindingExpression.ParentBinding;
+            myBinding.UpdateSourceExceptionFilter = ReturnExceptionHandler;
+            myBindingExpression.UpdateSource();
+        }
+
+        private void DisableCustomHandler(object sender, RoutedEventArgs e)
+        {
+            // textBox3 is an instance of a TextBox
+            // the TextProperty is the data-bound dependency property
+            var myBinding = BindingOperations.GetBinding(textBox3, TextBox.TextProperty);
+            myBinding.UpdateSourceExceptionFilter -= ReturnExceptionHandler;
+            BindingOperations.GetBindingExpression(textBox3, TextBox.TextProperty).UpdateSource();
+        }
+
+        private object ReturnExceptionHandler(object bindingExpression, Exception exception) => "This is from the UpdateSourceExceptionFilterCallBack.";
+
     }
 }
