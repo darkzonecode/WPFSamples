e225fa984aa9f6b852c83b453196e1bdbc7b1e97
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-11-22 13:16:08 -0500

    Implementing Data Conversion.

---
+++ WPFSamples/Data Binding/BindConversion/Converters/MyConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BindConversion.Converters
+{
+    public class MyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var date = (DateTime)value;
+
+            switch (targetType.Name)
+            {
+                case "String":
+                    return date.ToString("F", culture);
+
+                case "Brush":
+                    return Brushes.Red;
+
+                default:
+                    return value;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WPFSamples/Data Binding/BindConversion/Data/MyData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace BindConversion.Data
+{
+    public class MyData : INotifyPropertyChanged
+    {
+        private DateTime _theDate;
+
+        public MyData()
+        {
+            _theDate = DateTime.Now;
+        }
+
+        public DateTime TheDate
+        {
+            get { return _theDate; }
+            set
+            {
+                _theDate = value;
+                OnPropertyChanged("TheDate");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
