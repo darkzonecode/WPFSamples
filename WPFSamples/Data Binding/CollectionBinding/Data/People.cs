99ff7ec46f5cf8edf3c99cf71cb84733f4065470
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-11-24 00:49:24 -0500

    Simple collection binding.

---
+++ WPFSamples/Data Binding/CollectionBinding/Data/People.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace CollectionBinding.Data
+{
+    public class People : ObservableCollection<Person>
+    {
+        public People()
+        {
+            Add(new Person("Michael", "Alexander", "Bellevue"));
+            Add(new Person("Jeff", "Hay", "Redmond"));
+            Add(new Person("Christina", "Lee", "Kirkland"));
+            Add(new Person("Samantha", "Smith", "Seattle"));
+        }
+    }
+}
---
+++ WPFSamples/Data Binding/CollectionBinding/Data/Person.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CollectionBinding.Data
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string _firstname;
+        private string _hometown;
+        private string _lastname;
+
+        public Person()
+        {
+        }
+
+        public Person(string first, string last, string town)
+        {
+            _firstname = first;
+            _lastname = last;
+            _hometown = town;
+        }
+
+        public string FirstName
+        {
+            get { return _firstname; }
+            set
+            {
+                _firstname = value;
+                OnPropertyChanged("FirstName");
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastname; }
+            set
+            {
+                _lastname = value;
+                OnPropertyChanged("LastName");
+            }
+        }
+
+        public string HomeTown
+        {
+            get { return _hometown; }
+            set
+            {
+                _hometown = value;
+                OnPropertyChanged("HomeTown");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public override string ToString() => _firstname;
+
+        protected void OnPropertyChanged(string info)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
