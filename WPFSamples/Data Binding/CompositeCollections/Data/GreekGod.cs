810b918ef3772151662d58290b17c8ec6cbd9fc2
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-11-24 15:49:37 -0500

    Composite collection sample.

---
+++ WPFSamples/Data Binding/CompositeCollections/Data/GreekGod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositeCollections.Data
+{
+    public class GreekGod
+    {
+        public GreekGod(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+    }
+}
