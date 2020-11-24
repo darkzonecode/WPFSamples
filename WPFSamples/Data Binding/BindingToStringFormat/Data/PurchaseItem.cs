8b228cd92f5b1a29c0d07836c16b4f8532343039
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-11-23 21:00:40 -0500

    Binding to String format.

---
+++ WPFSamples/Data Binding/BindingToStringFormat/Data/PurchaseItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BindingToStringFormat.Data
+{
+    public class PurchaseItem
+    {
+        public PurchaseItem()
+        {
+
+        }
+
+        public PurchaseItem(string desc, double price, DateTime endDate)
+        {
+            Description = desc;
+            Price = price;
+            OfferExpires = endDate;
+        }
+
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public DateTime OfferExpires { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Description}, {Price}, {OfferExpires:D}";
+        }
+
+    }
+}
