d85d63ef95537624369f35a6caa2ac3c2c376418
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-12-15 02:27:38 -0500

    Add property change notifiction sample.

---
+++ WPFSamples/Data Binding/PropertyChangeNotification/Data/Bid.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PropertyChangeNotification.Data
+{
+    public class Bid : INotifyPropertyChanged
+    {
+        private string _bidItemName;
+        private decimal _bidItemPrice;
+
+        public Bid(string newBidItemName, decimal newBidItemPrice)
+        {
+            _bidItemName = newBidItemName;
+            _bidItemPrice = newBidItemPrice;
+        }
+
+
+        public string BidItemName
+        {
+            get { return _bidItemName; }
+            set 
+            {
+                if (_bidItemName.Equals(value) == false)
+                {
+                    _bidItemName = value;
+                    // Call OnPropertyChanged whenever the property is updated
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+        public decimal BidItemPrice
+        {
+            get { return _bidItemPrice; }
+            set
+            {
+                if(_bidItemPrice.Equals(value) == false)
+                {
+                    _bidItemPrice = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ WPFSamples/Data Binding/PropertyChangeNotification/Data/BidCollection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Timers;
+
+namespace PropertyChangeNotification.Data
+{
+    public class BidCollection : ObservableCollection<Bid>
+    {
+        private readonly Bid _item1 = new Bid("Perseus Vase", 24.95M);
+        private readonly Bid _item2 = new Bid("Hercules Statue", (decimal)16.05);
+        private readonly Bid _item3 = new Bid("Odysseus Painting", (decimal)100.0);
+
+        public BidCollection()
+        {
+            Add(_item1);
+            Add(_item2);
+            Add(_item3);
+            CreateTimer();
+        }
+
+        private void CreateTimer()
+        {
+            var timer = new Timer
+            {
+                Enabled = true,
+                Interval = 2000
+            };
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _item1.BidItemPrice += 1.25M;
+            _item2.BidItemPrice += 2.45M;
+            _item3.BidItemPrice += 10.55M;
+        }
+    }
+}
