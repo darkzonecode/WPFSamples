416522260a7e4232f187e60cb1fba7639177b7aa
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-12-15 16:35:11 -0500

    Add DataTrigger sample.

---
+++ WPFSamples/Data Binding/DataTrigger/Data/Place.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTrigger.Data
+{
+    public class Place
+    {
+        public Place(string name, string state)
+        {
+            Name = name;
+            State = state;
+        }
+
+        public string Name { get; set; }
+        public string State { get; set; }
+    }
+}
