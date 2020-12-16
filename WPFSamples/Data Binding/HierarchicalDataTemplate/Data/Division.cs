04c0248ed6af9c50024c6583d5cdf13550f667e1
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-12-16 15:34:03 -0500

    Add HierarchicalDataTemplate sample.

---
+++ WPFSamples/Data Binding/HierarchicalDataTemplate/Data/Division.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HierarchicalDataTemplate.Data
+{
+    public class Division
+    {
+        public Division(string name)
+        {
+            Name = name;
+            Teams = new List<Team>();
+        }
+
+        public string Name { get; }
+        public List<Team> Teams { get; }
+    }
+}
---
+++ WPFSamples/Data Binding/HierarchicalDataTemplate/Data/League.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HierarchicalDataTemplate.Data
+{
+    public class League
+    {
+        public League(string name)
+        {
+            Name = name;
+            Divisions = new List<Division>();
+        }
+
+        public string Name { get; }
+        public List<Division> Divisions { get; }
+    }
+}
