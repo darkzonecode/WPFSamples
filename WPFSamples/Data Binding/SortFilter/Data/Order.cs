0b8c544a9635f8ef45734cca15c416f093044501
Andriy Kostiv <AndriyKostiv@outlook.com>
2020-12-19 16:13:43 -0500

    Add sort filter example.

---
+++ WPFSamples/Data Binding/SortFilter/Data/Order.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SortFilter.Data
+{
+    public class Order : INotifyPropertyChanged
+    {
+        private int _customer;
+        private DateTime _datefilled;
+        private string _filled;
+        private int _id;
+        private string _name;
+        private int _order;
+        private DateTime _orderdate;
+
+        public Order(int order, int customer, string name, int id, string filled, DateTime orderdate,
+            DateTime datefilled)
+        {
+            OrderItem = order;
+            Customer = customer;
+            Name = name;
+            Id = id;
+            Filled = filled;
+            OrderDate = orderdate;
+            DateFilled = datefilled;
+        }
+
+        public Order(int order, int customer, string name, int id, string filled, DateTime orderdate)
+        {
+            OrderItem = order;
+            Customer = customer;
+            Name = name;
+            Id = id;
+            Filled = filled;
+            OrderDate = orderdate;
+
+        }
+
+        public int OrderItem
+        {
+            get { return _order; }
+            set
+            {
+                _order = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public int Customer
+        {
+            get { return _customer; }
+            set
+            {
+                _customer = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Filled
+        {
+            get { return _filled; }
+            set
+            {
+                _filled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public DateTime OrderDate
+        {
+            get { return _orderdate; }
+            set
+            {
+                _orderdate = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public DateTime DateFilled
+        {
+            get { return _datefilled; }
+            set
+            {
+                _datefilled = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPFSamples/Data Binding/SortFilter/Data/Orders.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace SortFilter.Data
+{
+    public class Orders : ObservableCollection<Order>
+    {
+        public Order O1 = new Order(1001, 5682, "Blue Sweater", 44, "Yes", new DateTime(2003, 1, 23),
+           new DateTime(2003, 2, 4));
+
+        public Order O2 = new Order(1002, 2211, "Gray Jacket Long", 181, "No", new DateTime(2003, 2, 14));
+
+        public Order O3 = new Order(1003, 5682, "Brown Pant W", 02, "Yes", new DateTime(2002, 12, 20),
+            new DateTime(2003, 1, 13));
+
+        public Order O4 = new Order(1004, 3143, "Orange T-shirt", 205, "No", new DateTime(2003, 1, 28));
+
+        public Orders()
+        {
+            Add(O1);
+            Add(O2);
+            Add(O3);
+            Add(O4);
+        }
+    }
+    
+}
