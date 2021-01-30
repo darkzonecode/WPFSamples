e7390c08fd26644b05cb96e9036cfd18ce6e2482
Andriy Kostiv <AndriyKostiv@outlook.com>
2021-01-29 22:49:25 -0500

    Add sample to alternate appearance of tree items.

---
+++ WPFSamples/StylesAndTemplates/AlternatingAppearanceOfItems/Data/Division.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlternatingAppearanceOfItems.Data
+{
+    public class Division
+    {
+        public Division(string name)
+        {
+            Name = name;
+            Teams = new List<Team>();
+        }
+
+
+        public string Name { get; }
+
+        public List<Team> Teams { get; }
+
+    }
+}
